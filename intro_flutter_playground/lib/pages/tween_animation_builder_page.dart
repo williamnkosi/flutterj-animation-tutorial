c70fee582e7801e45a551f657f3f9b46f9b59b1c
William Nkosi <william.dev.acc@gamil.com>
2022-03-05 01:02:41 -0600

    Created and learn't about the TweenAnimationBuilder

--- intro_flutter_playground/lib/pages/tween_animation_builder_page.dart
+++ intro_flutter_playground/lib/pages/tween_animation_builder_page.dart
@@ -20,19 +20,40 @@ class HSVColorSelector extends StatefulWidget {
 }
 
 class _HSVColorSelectorState extends State<HSVColorSelector> {
+  double _hue = 0.0;
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         TweenAnimationBuilder<double>(
-            tween: Tween<double>(begin: 0.0, end: 360.0),
-            duration: Duration(milliseconds: 1500),
+            tween: Tween<double>(begin: 0.0, end: _hue),
+            duration: Duration(milliseconds: 200),
             builder: (context, hue, child) {
               final hsvColor = HSVColor.fromAHSV(1.0, hue, 1.0, 1.0);
               return Container(
                   width: 200, height: 200, color: hsvColor.toColor());
             }),
+        SizedBox(
+          height: 40,
+        ),
+        Container(
+          height: 30,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            for (var h = 0; h <= 360; h++)
+              HSVColor.fromAHSV(1.0, h.toDouble(), 1.0, 1.0).toColor()
+          ], stops: [
+            for (var h = 0; h <= 360; h++) h.toDouble() / 360
+          ])),
+        ),
+        Slider.adaptive(
+            value: _hue,
+            min: 0,
+            max: 360,
+            onChanged: (value) => setState(() {
+                  _hue = value;
+                }))
       ],
     );
   }
